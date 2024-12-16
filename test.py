7b474d65a1a33e8fa0f78a936c8036a8943f1071
yinpeach <yinpeach0314@gmail.com>
2024-12-17 00:10:56 +0800

    deleted:    t.py 	deleted:    test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def optional_name_trans(s: str):
-    if (i := s.find('.get(')) != -1:
-        j = s.rfind(')')
-        s = s[:i] + f'[{s[i + 5:j]}]' + s[j + 1:]
-    return s
-
-print(optional_name_trans('data.get("tets")'))
